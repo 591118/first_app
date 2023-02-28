27529bf0da99de925528a9735c21aebe593a0d6b
Even Torbjørnsen <591118@stud.hvl.no>
2023-02-28 14:39:44 +0100

    rails first video

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  root to: "pages#home"
+
+  #GET localhost:3000 / about
+  get "/about", to:"pages#about"
+
+  get "/contact", to: "pages#contact"
+
 end
+
+
